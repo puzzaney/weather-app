a967b106d64c77ef5e9b2cafb3fa8447c28fc6fc
puzzaney <poojankarki12@gmail.com>
2024-09-03 20:59:53 +0545

    Initialized and added some elements to weather section

--- src/ui/WeatherMain.jsx
+++ src/ui/WeatherMain.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FaRegSun } from 'react-icons/fa';
 
 function WeatherMain() {
   const [city, setCity] = useState('');
@@ -11,6 +12,23 @@ function WeatherMain() {
         onChange={(e) => setCity(e.target.value)}
         value={city}
       />
+      <div className="grid grid-cols-2 mt-11 px-6 items-center">
+        <div className="flex flex-col">
+          <div className="mb-16">
+            <h2 className="font-bold text-4xl text-gray-400">Birtamode</h2>
+            <p className="mt-1 tracking-wider text-gray-400">
+              Chance of rain: 0%
+            </p>
+          </div>
+          <h1 className="font-bold text-6xl">31 &deg;</h1>
+        </div>
+        <div className=" text-9xl justify-self-end ">
+          <FaRegSun />
+        </div>
+        <div className="bg-secondary mt-9 px-4 pt-1.5 h-28 rounded-lg">
+          <h5 className="font-semibold">Today's Forecast</h5>
+        </div>
+      </div>
     </div>
   );
 }
